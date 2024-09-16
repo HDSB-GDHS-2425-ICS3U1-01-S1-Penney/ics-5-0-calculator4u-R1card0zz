4b0cbceb00341ba11719b226c0242316050f2703
R1card0zz <1correiaric@hdsb.ca>
2024-09-16 12:38:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+
 let num0 = 0;
+let num1 = 0;
 
 function loop (){
     num0 = prompt ("number");
+ num0 = Number(num0);
+    console.log ("Number Entered " + num0);
+
+if (num0 > 0);
+
+    num1 = promt ("second number");
+num1 = Number(num1);
+    console.log ("Numbers entered " + num1);
+    
+ 
+
+    
+    setTimeout(loop,2000);
 }
+loop();
